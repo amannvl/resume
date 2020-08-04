076d6a8e41aff114c1f3ab16c44e70c9ee05bbfa
Aman Sharma <47274283+amannvl@users.noreply.github.com>
2020-08-04 14:32:45 +0530

    Update settings.py

--- djangoResume/djangoResume/settings.py
+++ djangoResume/djangoResume/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bl(#yex^y^am7@r05)t-ly4+lz7eu4-co9j4(kyk7hhkglmfg$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
